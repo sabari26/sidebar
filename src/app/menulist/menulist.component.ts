049bcc844defce921970aba9fc4d2401fee6a220
Sabarinathan R <sabarir2@in.ibm.com>
2020-07-15 21:23:59 +0530

    style changes

--- src/app/menulist/menulist.component.ts
+++ src/app/menulist/menulist.component.ts
@@ -1,9 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit,ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-menulist',
   templateUrl: './menulist.component.html',
-  styleUrls: ['./menulist.component.css']
+  styleUrls: ['./menulist.component.css'],
+  encapsulation:ViewEncapsulation.None
 })
 export class MenulistComponent implements OnInit {
 
